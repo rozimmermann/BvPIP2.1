3a4091251a901f531778adddf5275ecf8b7df0a2
Ro Zimmermann <73970584+rozimmermann@users.noreply.github.com>
2025-11-05 22:25:23 -0300

    Trying with settings and load

--- src/data/load.py
+++ src/data/load.py
@@ -7,7 +7,9 @@ Data loading
 
 import yaml
 
-with open("config/settings.yaml", "r") as file:
-    config = yaml.safe_load(file)
-
-data_path = config["data_path"]
+with open("../../config/settings.yaml", "r") as file:
+    config = yaml.safe_load_all(file)
+    document1 = next(config)
+    document2 = next(config)
+    document3 = next(config)
+    document4 = next(config)
