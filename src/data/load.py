850ccc492939ead0d7aeb5c23968c5af89105e2e
Ro Zimmermann <73970584+rozimmermann@users.noreply.github.com>
2025-12-02 12:02:49 -0300

    Load generator edit and settings update
    Load generator edit and settings update for RMSD

--- src/data/load.py
+++ src/data/load.py
@@ -6,10 +6,11 @@ Data loading
 """
 
 import yaml
+import pandas as pd
 
 with open("../../config/settings.yaml", "r") as file:
     config = yaml.safe_load_all(file)
-    document1 = next(config)
-    document2 = next(config)
-    document3 = next(config)
-    document4 = next(config)
+    density = next(config)
+    PE = next(config)
+    Pf = next(config)
+    RMSD = next(config)
